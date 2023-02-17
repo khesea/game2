4c6eca3b19af34ff7770d67b045857ddfb2825d8
Ruslan <115349782+khesea@users.noreply.github.com>
2023-02-17 16:01:52 +0300

    files:

---
+++ script.js
@@ -0,0 +1,73 @@
+//обозначаем карты
+let cardOne = null;
+let cardTwo = null;
+let clickPermission = true;
+
+//обозначаем попытки
+let trying = document.getElementsByClassName('trying')[0].querySelector('span');
+
+//обозначаем успешные угадывания
+let successing = document.getElementsByClassName('successing')[0].querySelector('span');
+
+//обозначаем игровые карты
+const cards = document.getElementsByClassName("game-card");
+console.log(cards[0]);
+
+//добавляем иконку вопроса на все карточки
+for (let i = 0; i < cards.length; i++) {
+  cards[i].innerHTML += '<i class="fa fa-question-circle"></i>';
+}
+
+//функция клика на карточку
+for (let i = 0; i < cards.length; i++) {
+  cards[i].onclick = function () {
+    //если на нее кликнули и карта уже не отгадана, она поварачивается
+    if (clickPermission == true && !cards[i].classList.contains("okay")) {
+      cards[i].classList.add("flip");
+    //добавляем значение переменной кардВан в виде всего дива этой карты
+      if (cardOne == null) {
+        cardOne = i;
+      } else {
+      //далее на следующий нажим cardOne уже не нулл, он равен значению i
+      //если значение i новонажатой карты не равно кардВан, то i присуждается переменной cardTwo
+        if (i != cardOne) {
+          cardTwo = i;
+          clickPermission = false;
+        }
+      }
+
+    //далее идёт проверка нажатых карт
+      if (cardOne != null && cardTwo != null && cardOne != cardTwo) {
+        //если значения названий классов первого элемента сопадают со второй карточкой
+        if (
+          cards[cardOne].firstElementChild.className ===
+          cards[cardTwo].firstElementChild.className
+        ) {
+          //то за полсекунды происходит следующее:
+          setTimeout(() => {
+            //обеим картам присуждается новый класс с зеленым фоном
+            cards[cardOne].classList.add("okay");
+            cards[cardTwo].classList.add("okay");
+            //также обнуляются переменные cardOne и cardTwo и плюсуется счётчик
+            cardOne = null;
+            cardTwo = null;
+            clickPermission = true;
+            successing.innerHTML = parseInt(successing.innerText) + 1;
+          }, 500);
+        } else {
+          //если значения не совпадают, то обе карты переворачиваются и значения переменных обнуляются
+          setTimeout(() => {
+            cards[cardOne].classList.remove("flip");
+            cards[cardTwo].classList.remove("flip");
+
+            cardOne = null;
+            cardTwo = null;
+            clickPermission = true;
+          }, 1000);
+        }
+        //неважно угадал-не угадал, счётчик попыток прибавится
+        trying.innerHTML = parseInt(trying.innerText) + 1;
+      }
+    }
+  };
+}
